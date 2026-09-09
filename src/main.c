59a15001c2bd462795240f6772e3c95743cc0af7
gustavo-hsilva79 <gustavo.hsilva79@senacsp.edu.br>
2026-09-09 20:26:51 -0300

    refactor: move entry point to src/main.c

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+int main(void)
+{
+    /* Código inicial do protótipo será desenvolvido aqui. */
+    return 0;
+}
